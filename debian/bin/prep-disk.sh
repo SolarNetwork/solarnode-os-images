5e1735c9bd70847b73db983330881fbfd487475a
Matt Magoffin <matt@solarnetwork.net>
2019-07-19 12:16:53 +1200

    Add support for loopback device partitions.

--- debian/bin/prep-disk.sh
+++ debian/bin/prep-disk.sh
@@ -11,7 +11,14 @@
 # shrinks the /boot partition to 128M and / to 822M via an intermediate 840M:
 #
 #   prep-disk.sh -d /dev/sde -f 128M -g '-132M' -S 840M -s 822M -b 244224
-
+#
+# Some more examples, for ever smaller /boot partitions:
+# 
+#   prep-disk.sh -d /dev/sde -f 96M -g '-164M' -s 854M -b 244224
+#   prep-disk.sh -d /dev/sde -f 64M -g '-196M' -s 886M -b 244224
+#
+# A loopback device can be used if you specify the -P argument like '-P p'
+# so the partition names correctly resolve like "p0" and "p1".
 
 DRYRUN=
 VERBOSE=0
@@ -25,9 +32,10 @@ FAT_PART="1"
 FAT_SIZE=""
 FAT_MOUNT=
 PART_SHIFT=""
+PART_PREFIX=""
 
 
-while getopts ":b:d:F:f:g:kno:p:s:v" opt; do
+while getopts ":b:d:F:f:g:kno:P:p:s:v" opt; do
 	case $opt in
 		b) OUTBLKS="$OPTARG" ;;
 		d) DISK="$OPTARG" ;;
@@ -36,6 +44,7 @@ while getopts ":b:d:F:f:g:kno:p:s:v" opt; do
 		g) PART_SHIFT="$OPTARG" ;;
 		n) DRYRUN=1 ;;
 		o) OUTFILE="$OPTARG" ;;
+		P) PART_PREFIX="$OPTARG" ;;
 		p) PART="$OPTARG" ;;
 		S) SIZE0="$OPTARG" ;;
 		s) SIZE="$OPTARG" ;;
@@ -50,66 +59,69 @@ if [ ! -e "$DISK" ]; then
 	exit 1
 fi
 
-if [ ! -e "$DISK$PART" ]; then
-	echo "Partition $DISK$PART not available."
+partDev=$DISK$PART_PREFIX$PART
+fatPartDev=$DISK$PART_PREFIX$FAT_PART
+
+if [ ! -e "$partDev" ]; then
+	echo "Partition $partDev not available."
 	exit 1
 fi
 
 if [ -n "$FAT_SIZE" ];then
-	if [ ! -e "$DISK$FAT_PART" ]; then
-		echo "FAT partition $DISK$FAT_PART not available."
+	if [ ! -e "$fatPartDev" ]; then
+		echo "FAT partition $fatPartDev not available."
 		exit 1
 	fi
 	tmp_dir=$(mktemp -d -t fat-XXXXX)
-	echo "Mounting $DISK$FAT_PART -> $tmp_dir"
+	echo "Mounting $fatPartDev -> $tmp_dir"
 	if [ -z $DRYRUN ]; then
-		sudo mount $DISK$FAT_PART $tmp_dir || exit 1
+		sudo mount $fatPartDev $tmp_dir || exit 1
 	fi
 
 	tmp_data=$(mktemp -t fat-data-XXXXX.tgz)
-	echo "Saving $DISK$FAT_PART data -> $tmp_data"
+	echo "Saving $fatPartDev data -> $tmp_data"
 	if [ -z $DRYRUN ]; then
 		sudo tar czf $tmp_data -C $tmp_dir . || exit 1
 	fi
 	
-	echo "Resizing $DISK$FAT_PART partition to $FAT_SIZE..."
+	echo "Resizing $fatPartDev partition to $FAT_SIZE..."
 	if [ -z $DRYRUN ]; then
 		sudo umount $tmp_dir || exit 1
 		echo "- $FAT_SIZE" |sudo sfdisk $DISK -N$FAT_PART --no-reread --no-tell-kernel --force
 		sudo partx -u $DISK
 	fi
 
-	echo "Restoring $DISK$FAT_PART data <- $tmp_data"
+	echo "Restoring $fatPartDev data <- $tmp_data"
 	if [ -z $DRYRUN ]; then
-		sudo mkfs.fat -F 32 -n boot $DISK$FAT_PART || exit 1
-		sudo mount $DISK$FAT_PART $tmp_dir || exit 1
+		sudo mkfs.fat -F 32 -n boot $fatPartDev || exit 1
+		sudo mount $fatPartDev $tmp_dir || exit 1
 		sudo tar xf $tmp_data -C $tmp_dir || exit 1
 		sudo umount $tmp_dir
 	fi
 fi
 
 if [ -n "$SIZE0" ]; then
-	echo "Resizing filesystem on $DISK$PART to $SIZE0..."
+	echo "Resizing filesystem on $partDev to $SIZE0..."
 	if [ -z "$DRYRUN" ]; then
-		sudo resize2fs -fp "$DISK$PART" "$SIZE0" || exit 1
+		sudo resize2fs -fp "$partDev" "$SIZE0" || exit 1
 		sudo sync
 	fi
 fi
 
-echo "Resizing filesystem on $DISK$PART to $SIZE..."
+echo "Resizing filesystem on $partDev to $SIZE..."
 if [ -z "$DRYRUN" ]; then
-	sudo resize2fs -fp "$DISK$PART" "$SIZE" || exit 1
+	sudo resize2fs -fp "$partDev" "$SIZE" || exit 1
 	sudo sync
 fi
 
-echo "Resizing $DISK$PART partition to $SIZE..."
+echo "Resizing $partDev partition to $SIZE..."
 if [ -z $DRYRUN ]; then
 	echo "- $SIZE" |sudo sfdisk $DISK -N$PART --no-reread --no-tell-kernel --force
 	sudo partx -u $DISK
 fi
 
 if [ -n "$PART_SHIFT" ];then
-	echo "Shifting $DISK$PART partition $PART_SHIFT..."
+	echo "Shifting $partDev partition $PART_SHIFT..."
 	if [ -z $DRYRUN ]; then
 		echo "$PART_SHIFT," |sudo sfdisk --move-data $DISK -N$PART --no-reread --no-tell-kernel --force
 		sudo partx -u $DISK
