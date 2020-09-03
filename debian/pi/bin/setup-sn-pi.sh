2a8983ef37df80c545f028f0fcb316c086adc823
Matt Magoffin <matt@solarnetwork.net>
2020-09-03 12:48:37 +1200

    Make sure SSH enabled.

--- debian/pi/bin/setup-sn-pi.sh
+++ debian/pi/bin/setup-sn-pi.sh
@@ -482,6 +482,13 @@ setup_startup_screen () {
 	fi
 }
 
+setup_ssh () {
+	if ! systemctl is-active ssh >/dev/null; then
+		echo -n "Enabling ssh... "
+		systemctl enable ssh && echo 'OK' || echo 'ERROR'
+	fi
+}
+
 setup_software_early
 setup_hostname
 setup_dns
@@ -504,4 +511,5 @@ setup_issue
 if [ -z "$SKIP_SOFTWARE" ]; then
 	setup_software_late
 fi
+setup_ssh
 check_err
