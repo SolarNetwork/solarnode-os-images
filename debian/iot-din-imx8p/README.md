e1f95429e7837203116b6e5ee2421aac5465d88c
Matt Magoffin <matt@solarnetwork.net>
2025-03-19 08:02:54 +1300

    Start work on IoT DIN iMX8p OS image setup.

---
+++ debian/iot-din-imx8p/README.md
@@ -0,0 +1,27 @@
+# SolarNode IOT-DIN-iMX8p Images
+
+Here's an example of creating a SolarNodeOS 12 image out of a Debian `bookworm` IOT-DIN-iMX8p source
+image:
+
+```sh
+sudo ../bin/customize.sh -v -z \
+	-N 1 -n 2 -c -M /boot -U \
+	-E 2736 \
+	-a '-a iotdin-imx8p -V compulab -E -M 12 -q bookworm -m -w -Q -D conf/packages-deb12-del-early.txt -K conf/packages-deb12-add.txt -k conf/packages-deb12-keep.txt -X bin/extra-early.sh -x bin/extra-late.sh -o 172.16.159.167:3142' \
+	-o /var/tmp/solarnodeos-deb12-iotdin_imx8p-2GB-$(date '+%Y%m%d').img \
+	/var/tmp/debian-bookworm-arm64_iotdin-imx8p_live-img.img \
+	../bin/setup-sn.sh \
+	$PWD:/tmp/overlay
+```
+
+# Copying to eMMC
+
+To copy the image to the eMMC on an IoT device
+
+1. Connect a USB cable between the micro-USB "Console" serial port on the IoT and your computer
+2. Connect to the USB console with a terminal emulator, e.g. `screen /dev/tty.usbserial-0236DBD7 115200`
+3. Copy the image to a USB stick, plug the USB stick into the IoT, power it on
+4. The IoT will boot SolarNodeOS; log in as `solar/solar`
+5. Run `sudo sn-stop` to shut down SolarNode 
+6. Run `sudo rm -f /etc/ssh/ssh_host*` to remove `ssh` host keys, so not copied to eMMC.
+7. Run `sudo cl-deploy` and follow the prompts
---
+++ debian/iot-din-imx8p/bin/extra-early.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env sh
+
+if [ $(id -u) -ne 0 ]; then
+	echo "This script must be run as root."
+	exit 1
+fi
+
+DRY_RUN=""
+VERBOSE=""
+
+do_help () {
+	cat 1>&2 <<EOF
+Usage: $0 <arguments>
+
+Extra setup script IoT Gate SolarNodeOS.
+
+Arguments:
+ -n                     - dry run; do not make any actual changes
+ -v                     - verbose mode; print out more verbose messages
+EOF
+}
+
+while getopts ":nv" opt; do
+	case $opt in
+		n) DRY_RUN='TRUE';;
+		v) VERBOSE='TRUE';;
+		*)
+			echo "Unknown argument ${OPTARG}"
+			do_help
+			exit 1
+	esac
+done
+shift $(($OPTIND - 1))
+
+# Fix bug in bt-start removal scripts
+if grep -q 'disable bt-start.service$' /var/lib/dpkg/info/bt-start.postrm 2>/dev/null; then
+	echo -n "Fixing bt-start package remove script /var/lib/dpkg/info/bt-start.postrm... "
+	if [ -n "$DRY_RUN" ]; then
+		echo "DRY RUN"
+	elif sed -i -e 's/disable bt-start.service$/disable bt-start.service 2>\/dev\/null || true/' /var/lib/dpkg/info/bt-start.postrm; then
+		echo "OK"
+	else
+		echo "ERROR"
+	fi
+fi
+
+# remove backports
+if grep -q 'backports' /etc/apt/sources.list; then
+	echo -n "Removing backports apt source... "
+	if [ -n "$DRY_RUN" ]; then
+		echo "DRY RUN"
+	elif sed -i -e '/backports/d' /etc/apt/sources.list; then
+		echo "OK"
+	else
+		echo "ERROR"
+	fi
+fi
+
+# remove node repo
+if [ -e /etc/apt/sources.list.d/nodesource.list ]; then
+	echo -n "Removing nodesource apt source... "
+	if [ -n "$DRY_RUN" ]; then
+		echo "DRY RUN"
+	elif rm -f /etc/apt/sources.list.d/nodesource.list; then
+		echo "OK"
+	else
+		echo "ERROR"
+	fi
+fi
+
+# mark packages as manually installed so autoremove does not remove
+for f in iproute2 iw linux-base; do	
+	if apt-mark showauto |grep -q $f 2>/dev/null; then
+		echo -n "Marking $f package as maually installed... "
+		if [ -n "$DRY_RUN" ]; then
+			echo "DRY RUN"
+		elif apt-mark manual $f >/dev/null; then
+			echo "OK"
+		else
+			echo "ERROR"
+		fi
+	fi
+done
